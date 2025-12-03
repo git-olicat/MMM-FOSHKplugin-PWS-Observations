a1dc0f82fafe7160961b0b488f41f87df543ff31
root <root@oli-ubuntu2.localnet>
2025-12-03 15:52:36 +0100

    sundata

--- MMM-FOSHKplugin-PWS-Observations.js
+++ MMM-FOSHKplugin-PWS-Observations.js
@@ -1347,7 +1347,6 @@ Module.register("MMM-FOSHKplugin-PWS-Observations", {
       table_sitrep.appendChild(row_sitrep);
     }
 
-    /*
     if (this.config.sunRise == "1"){
       var row_sitrep = document.createElement("tr");
       var Icon = document.createElement("td");
@@ -1413,7 +1412,6 @@ Module.register("MMM-FOSHKplugin-PWS-Observations", {
       row_sitrep.appendChild(Value);
       table_sitrep.appendChild(row_sitrep);
     }
-    */
 
     console.log("table" + table_sitrep);
     wrapper.appendChild(table_sitrep);
